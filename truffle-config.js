a087a6c6bbe0a645a19359671ced7e72b6f6675d
Kakashi-Of-Saringan <shaktawatdevendra03@gmail.com>
2019-09-16 08:04:36 +0530

    file adjusted for deploying contract to ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
+var HDWalletProvider = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+var mnemonic = "gym rug lyrics spin emerge among skin jewel then soccer wing suit";
 
 module.exports = {
   /**
@@ -60,14 +60,19 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
+     ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/cbdf015c731d4f0b8feef64b73a780df")
+      },
+      network_id: 3,
+      //gas: 5500000
       // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
       // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      // Ropsten has a lower block limit than mainnet
       // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+     },
 
     // Useful for private networks
     // private: {
